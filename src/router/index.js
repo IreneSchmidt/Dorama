a31c0f3077230b8898fcdae1a463040c75224f8a
thiago carvalho <101213387+guinho0@users.noreply.github.com>
2025-02-09 15:34:57 -0300

    Revertendo alterações do commit db88da8

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView.vue'
+import DoramasView from '../views/DoramasView.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/doramas'
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
+  {
+    path: '/doramas',
+    name: 'doramas',
+    component: DoramasView,
+    meta: { requiresAuth: true }
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
