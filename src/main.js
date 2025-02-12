216872223a9f343471193fb0036396fe5e1f168a
KaioHenr <120226915+KaioHenr@users.noreply.github.com>
2025-02-12 02:54:06 -0300

    bugFix: correção main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import router from "./router"; // Se estiver usando Vue Router
+import router from "./router/index";
 
 const app = createApp(App);
 const pinia = createPinia();
